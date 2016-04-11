ebda47f81b40b4460bb88c0ed93bf6668d5ba96a
Housseini Toumani <housseini.toumani@ysance.com>
2016-04-11 14:54:10 +0200

    remove .idea and update

--- app.js
+++ app.js
@@ -4,5 +4,11 @@
 angular.module('mofaApp', [])
   .controller('MainCtrl', function() {
     this.query = "";
+    this.vowels = mofafegoli.vowels;
     this.convert = mofafegoli.convert;
+    this.consonants = mofafegoli.consonants;
+    this.hasValue = function(aSyllabe){
+      var word = !isNaN(parseInt(this.query)) ? this.convert(this.query) : this.query;
+      return word.includes(aSyllabe);
+    }
   });
